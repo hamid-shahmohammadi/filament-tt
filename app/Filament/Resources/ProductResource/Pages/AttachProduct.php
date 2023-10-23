90642e918ef5fc594f7bb371e0889ae1ebca2517
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2023-10-23 19:33:40 +0330

    add dropzone template

--- app/Filament/Resources/ProductResource/Pages/AttachProduct.php
+++ app/Filament/Resources/ProductResource/Pages/AttachProduct.php
@@ -2,21 +2,30 @@
 
 namespace App\Filament\Resources\ProductResource\Pages;
 
-use App\Filament\Resources\ProductResource;
 use App\Models\Product;
+use Livewire\Attributes\Rule;
+use Livewire\WithFileUploads;
 use Filament\Resources\Pages\Page;
+use App\Filament\Resources\ProductResource;
 
 class AttachProduct extends Page
 {
+    use WithFileUploads;
+
     protected static string $resource = ProductResource::class;
 
     protected static string $view = 'filament.resources.product-resource.pages.attach-product';
 
     public $product;
 
+    #[Rule('image|max:1024')] // 1MB Max
+    public $photo;
+
     public function mount($record){
         $this->product=Product::find($record);
     }
 
-    
+    public function uploadFile(){
+        dd($this->photo->getSize());
+    }
 }
