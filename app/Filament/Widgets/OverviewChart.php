043633e540f94d522f7baaf54fa458561fcd1a76
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2023-11-06 23:38:48 +0330

    add widget chart

---
+++ app/Filament/Widgets/OverviewChart.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Filament\Widgets\ChartWidget;
+
+class OverviewChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(User::class)
+        ->between(
+            start: now()->startOfMonth(),
+            end: now()->endOfMonth(),
+        )
+        ->perDay()
+        ->count();
+
+    return [
+        'datasets' => [
+            [
+                'label' => 'User Register',
+                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+            ],
+        ],
+        'labels' => $data->map(fn (TrendValue $value) => $value->date),
+    ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
