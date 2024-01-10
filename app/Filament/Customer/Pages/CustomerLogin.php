2659c0d18fa1bd7e51afa5e729b075dd1f48ac7d
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2024-01-10 23:56:06 +0330

    add customer panel

---
+++ app/Filament/Customer/Pages/CustomerLogin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Customer\Pages;
+use Filament\Pages\Auth\Login as AuthLogin;
+
+use Filament\Pages\Page;
+
+class CustomerLogin extends AuthLogin
+{
+    // protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    // protected static string $view = 'filament.customer.pages.customer-login';
+}
